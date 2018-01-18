9c7d7476c3a41b13fc06be57633338fdcbadd86d
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-18 19:47:32 +0100

    tabbed content

--- main.js
+++ main.js
@@ -116,3 +116,19 @@ searchBar.addEventListener('keyup', function(e) {
     }
   });
 });
+
+var tabs = document.querySelector('.tabs');
+var panels = document.querySelectorAll('.panel')
+
+tabs.addEventListener('click', function(e) {
+  if(e.target.tagName == 'LI' ) {
+    var targetPanel = document.querySelector(e.target.dataset.trgt)
+    panels.forEach(function(panel) {
+      if(panel == targetPanel) {
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
