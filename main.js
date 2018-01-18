908c1b12cc85118ff188c3f23581e9ada8bb409c
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-18 18:28:23 +0100

    custom search filter

--- main.js
+++ main.js
@@ -102,3 +102,17 @@ hideBox.addEventListener('change', function(e) {
     list.style.display = 'initial'
   }
 });
+
+var searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e) {
+  var term = e.target.value.toLowerCase();
+  var books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book) {
+    var title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1) {
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
