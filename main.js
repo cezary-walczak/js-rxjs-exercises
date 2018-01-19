3180b708039b6a40f5f26a1d46475897de5d6af6
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-19 11:42:32 +0100

    spread operator

--- main.js
+++ main.js
@@ -25,4 +25,15 @@ window.onload = function() {
     console.log(name, belt, age);
   }
   log();
+
+  var cities = ['London', 'Paris', 'Berlin'];
+  console.log(...cities);
+  var nums1 = [1, 2, 3];
+  var nums2 = [...nums1, 4, 5, 6];
+  console.log(nums2);
+  var nums = [3, 5, 7]
+  function addNums(a, b, c) {
+    console.log(a + b + c);
+  }
+  addNums(...nums);
 }
