03cd9a644428195de55b1cf4f76bd18a5f6f5146
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-21 20:27:40 +0100

    subject

--- main.js
+++ main.js
@@ -43,3 +43,27 @@ observableInterval
   })
   .throttleTime(2000)
   .subscribe(observer);
+
+
+var subject = new Rx.Subject();
+
+subject.subscribe({
+  next: function(value) {
+    console.log(value);
+  },
+  error: function(error) {
+    console.log(error);
+  },
+  complete: function() {
+    console.log('complete');
+  }
+})
+
+subject.subscribe({
+  next: function(value) {
+    console.log(value);
+  }
+})
+
+subject.next('new data piece');
+subject.complete();
